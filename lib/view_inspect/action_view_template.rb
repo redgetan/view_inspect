dda0ccea0fd0549bd53b0e0127b7d86b0960591a
Reginald Tan <redge.tan@gmail.com>
2014-11-02 10:07:31 -0500

    move comment

--- lib/view_inspect/action_view_template.rb
+++ lib/view_inspect/action_view_template.rb
@@ -17,16 +17,16 @@ module ViewInspect
       ::ActionView::Template.class_eval do
         alias_method :orig_initialize, :initialize
 
+        # ActionView Template Resolver uses File.binread to read views
+        # thus making ASCII-8BIT (alias for binary) the default encoding.
+        #
+        # libxml2 which nokogiri uses doesnt support ASCII-8BIT encoding.
+        # That's why we need to encode first before letting nokogiri parse
+        # html fragment and add file:line information to DOM nodes
         def initialize(source, identifier, handler, details)
           orig_initialize(source, identifier, handler, details)
 
           if handler.respond_to? :erb_implementation
-            # ActionView Template Resolver uses File.binread to read views
-            # thus making ASCII-8BIT (alias for binary) the default encoding.
-            #
-            # libxml2 which nokogiri uses doesnt support ASCII-8BIT encoding.
-            # That's why we need to encode first before letting nokogiri parse
-            # html fragment and add file:line information to DOM nodes
             encode!
             @source = Handlers::ERB.new.add_file_line_to_html_tags(@source, identifier)
           end
